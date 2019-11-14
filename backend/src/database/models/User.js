7a884b0f34d1f71d41bf8ff375be2dc94526bced
Vitalik Pylypets <v.pylypets94@gmail.com>
2019-11-14 14:18:20 +0100

    Create user

---
+++ backend/src/database/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const shema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate(email) {
+      const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!emailRegex.test(email)) {
+        throw new Error('invalid email');
+      }
+    },
+  },
+});
+
+const User = mongoose.model('user', shema);
+
+module.exports = User;
